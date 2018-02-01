cac011971fad0b2350dc1f09de02135d5e47b46f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-01 11:15:14 +0100

    test: cleanup

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import 'cutaway'
 import { report, assert } from 'tapeless'
-import turtle from './index.es'
+import createTaxi from './index.mjs'
 
 const { ok } = assert
 
 try {
-  turtle()
+  createTaxi()
 } catch (e) {
   ok(e instanceof Error, 'error', 'will throw sans context')
   ok(e.message === 'Invalid rendering context', 'error message is a match')
