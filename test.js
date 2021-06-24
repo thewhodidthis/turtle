4b49e1ca9295e2faa8fff917789911da1d7b843a
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-24 21:18:54 +0100

    test: use double quotes

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import 'cutaway'
-import { report, assert } from 'tapeless'
-import createTaxi from './index.mjs'
+import "cutaway"
+import { assert, report } from "tapeless/browser"
+import createTaxi from "./main.js"
 
 const { ok } = assert
 
@@ -8,10 +8,10 @@ try {
   createTaxi()
 } catch (e) {
   ok
-    .describe('will throw sans context')
-    .test(e instanceof Error, 'error')
-    .describe('error message is a match')
-    .test(e.message === 'Invalid rendering context')
+    .describe("will throw sans context")
+    .test(e instanceof Error, "error")
+    .describe("error message is a match")
+    .test(e.message === "Invalid rendering context")
 }
 
 report()
